75931610d15b1a62d51161a58f479b70fc7dbedd
MatthewCKemp <matthewconorkemp@gmail.com>
2016-06-27 02:04:35 +0100

    Welcome V0.4

--- Welcome.php
+++ Welcome.php
@@ -1,15 +1,23 @@
+<!doctype html>
+<html lang=en">
+    <head>
+        <meta charset=""UTF-8">
+        <title>Test</title>
+    </head>
+        <body>
+            <?php
+            $username = "b56f549a76a983";
+            $password = "a3035583";
+            $servername = "us-cdbr-azure-west-c.cloudapp.net";
 
-<?php
-$username = "b56f549a76a983";
-$password = "a3035583";
-$servername = "us-cdbr-azure-west-c.cloudapp.net";
+            // Create connection to DB
+            $conn = mysqli_connect($servername, $username, $password);
 
-// Create connection to DB
-$conn = mysqli_connect($servername, $username, $password);
-
-// Check the connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-?>
+            // Check the connection
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+            echo "Connected successfully";
+            ?>
+        </body>
+</html>
