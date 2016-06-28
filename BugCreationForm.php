3dfdb7b644b0eac3aa54f6d50fbaa53b69688d3f
MatthewCKemp <matthewconorkemp@gmail.com>
2016-06-28 03:26:27 +0100

    BCF_V.0.3

--- BugCreationForm.php
+++ BugCreationForm.php
@@ -2,7 +2,7 @@
 <html lang=en">
     <head>
         <meta charset="UTF-8">
-        <title>BugSplat: Create a New Bug</title>
+        <title>BugSplat:Create a New Bug</title>
     </head>
         <body>
             <?php
