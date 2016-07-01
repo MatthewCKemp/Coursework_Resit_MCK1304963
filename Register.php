c844d445f6a95f8daf696bc6c5a6cca1f4ca3e8d
MatthewCKemp <matthewconorkemp@gmail.com>
2016-07-01 03:49:49 +0100

    Register_V.0.1

---
+++ Register.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang=en">
+<head>
+    <meta charset="UTF-8">
+    <title>BugSplat Registration</title>
+</head>
+<body>
+
+<form action="Register.php" method="post">
+    Please fill in the following details to create your account:<br>
+    Name (This will become your username): <input type="text" name="U_name"><br>
+    Home Country: <input type="text" name="U_country"><br>
+    <input type="submit" value ="Submit "/><br/>
+</form>
+
+<?php
+$username = "b56f549a76a983";
+$password = "a3035583";
+$servername = "us-cdbr-azure-west-c.cloudapp.net";
+
+// Create connection to DB
+$conn = mysqli_connect($servername, $username, $password);
+$select = mysqli_select_db($conn, 'mck1304963_cwrs_db');
+// Check the connectionas
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    //echo "Connected successfully";
+    if (!$select) {
+        die(" Selection failed: " . mysqli_connect_error());
+    }
+    //echo " DB Selected successfully";
+?>
+    <?php echo "Title: " . $_POST["B_title"]; ?><br>
+    <?php echo "Description: " .$_POST["B_Desc"]; ?><br>
+<?php   
+    $UserName = mysqli_real_escape_string($conn, $_POST["U_name"]);
+    $UserCountry = mysqli_real_escape_string($conn, $_POST["U_country"]);
+
+    $UserInsert = "INSERT INTO Users (name, country) VALUES ('$UserName', '$UserCountry')";
+        if(mysqli_query($conn, $UserInsert)){
+            echo " Your account has been created successfully .";
+        
+        
+        
+        
+        
+        
+        
+        
+        } else {
+            echo " ERROR: Unable to create account" . mysqli_error($conn);
+        }
+mysqli_close($conn);
+?>
+</body>
+</html>
