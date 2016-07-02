847882395b9d821b3d9c22eae2a60ae227acc928
MatthewCKemp <matthewconorkemp@gmail.com>
2016-07-02 06:15:05 +0100

    Login_V.2.6

--- Login.php
+++ Login.php
@@ -11,7 +11,7 @@
         <form action="Login.php" method="post">
             Username: <input type="text" name="username"><br>
             Password: <input type="password" name="password"><br>
-            <input type="submit" value ="Submit "/><br/>
+            <input type="submit" value ="Login"/><br/>
         </form>
     </div>
     <div id="navigation">
