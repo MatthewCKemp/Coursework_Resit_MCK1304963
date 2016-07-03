ceab2a9f14914d223d5573e52c803a397ea2fbc3
MatthewCKemp <matthewconorkemp@gmail.com>
2016-07-03 04:45:12 +0100

    Welcome_V.3.6

--- Welcome.php
+++ Welcome.php
@@ -42,7 +42,7 @@
                         if (mysqli_num_rows($result) > 0) {
                             echo "<table><tr><th>Bug Name</th><th>Date Posted</th></tr>";
                             while ($rows = mysqli_fetch_assoc($result)) { //Outputs data in each row.
-                                echo "<tr><td>" . $rows["title"] . $rows["bugposted"] . "</tr></td>";
+                                echo "<tr><td>" . $rows["title"] . "</td><td>" . $rows["bugposted"] . "</tr></td>";
                             }
                             echo"</table>";
                         } else {
