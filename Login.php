93d7b85b66b1340ebc815be40caa9bd01f43ed2a
MatthewCKemp <matthewconorkemp@gmail.com>
2016-07-04 02:14:28 +0100

    Login_V.2.8

--- Login.php
+++ Login.php
@@ -16,12 +16,13 @@
     </div>
     <div id="navigation">
         <p><strong>Links</strong></p>
-        <a href="http://mck1304963cwresit.azurewebsites.net/Welcome.php">Return to the welcome page</a>
-        <a href="http://mck1304963cwresit.azurewebsites.net/Register.php">Create a new account</a>
-        <a href="http://mck1304963cwresit.azurewebsites.net/BugCreationForm.php">Create a new Bug</a>
-        <a href="http://mck1304963cwresit.azurewebsites.net/Search.php">Search</a>
-    </div>
-    <div id="content">
+        <ul>
+            <li><a href="http://mck1304963cwresit.azurewebsites.net/Welcome.php">Return to the welcome page</a></li>
+            <li><a href="http://mck1304963cwresit.azurewebsites.net/Register.php">Create a new account</a></li>
+            <li><a href="http://mck1304963cwresit.azurewebsites.net/BugCreationForm.php">Create a new Bug</a></li>
+            <li><a href="http://mck1304963cwresit.azurewebsites.net/Search.php">Search</a></li>
+        </ul>
+        <div id="content">
         <p><strong>Main writing</strong></p>
     </div>
     <div id="comments">
