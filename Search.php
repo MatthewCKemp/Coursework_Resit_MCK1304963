6c3dbcd2570e293e1880430d351d5154b2b82ead
MatthewCKemp <matthewconorkemp@gmail.com>
2016-07-07 03:15:03 +0100

    Search_V.0.5

--- Search.php
+++ Search.php
@@ -39,9 +39,9 @@
 
             $keywords = "SELECT key_description FROM Keywords";
 
-            echo "<option name=Keywords>Keyword</option>";
+            echo "<select name=Keywords>Keyword</select>";
             foreach ($conn->query($keywords) as $row){
-                echo "<option value=$row[key_description]></option>";
+                echo "<option value=$row[key_description]>$row[key_ID]</option>";
             }
 
             echo "</select>";
