33fe3f0f73636b0d25d0d1e9c9c8dec5e67c2ec1
MatthewCKemp <matthewconorkemp@gmail.com>
2016-07-08 03:44:58 +0100

    Login_V.2.17

--- Login.php
+++ Login.php
@@ -4,7 +4,6 @@
         <meta charset="UTF-8">
         <title>BugSplat Login</title>
         <link rel="stylesheet" type ="text/css" href="layout.css" />
-
         <?php
         $username = "b56f549a76a983";
         $password = "a3035583";
@@ -22,54 +21,51 @@
             die(" Selection failed: " . mysqli_connect_error());
         }
         //echo " DB Selected successfully";
+
         ?>
     </head>
-    <body>
-        <div id ="header">Login</div>
-        <div id="login">
-            <form action="Login.php" method="post">
-                Username: <input type="text" name="username"><br>
-                Password: <input type="password" name="password"><br>
-                <input type="submit" value ="Login"/><br/>
-            </form>
-
-            <?php echo "Username: " . $_POST["username"]; ?><br>
-            <?php echo "Password: " .$_POST["password"];   
-            
-            $name = mysqli_real_escape_string($conn,$_POST['username']);
-            $Pass = mysqli_real_escape_string($conn,$_POST['password']);
+        <body>
+            <div id ="header">Login</div>
+            <div id="login">
+                <form action="Login.php" method="post">
+                    Username: <input type="text" name="username"><br>
+                    Password: <input type="password" name="password"><br>
+                    <input type="submit" value ="Login"/><br/>
+                </form>
     
-            $Search = "SELECT user_ID, name FROM Users WHERE name = '$name' AND user_ID = '$Pass'";
-            $row = mysqli_fetch_array($Search) or die(mysqli_error($conn));
+                <?php echo "Username: " . $_POST["username"]; ?><br>
+                <?php echo "Password: " .$_POST["password"];
     
-                if(!empty($row['username']) AND !empty($row['password'])) {
-                    $_SESSION['Login'] = $row['username'];
-                    echo "correct login";
+                $name = mysqli_real_escape_string($conn,$_POST['username']);
+                $Pass = mysqli_real_escape_string($conn,$_POST['password']);
     
-                }else {
-                    echo "Incorrect login, please retry";
-                }
-                mysqli_close($conn);
-            ?>
-        </div>
-        <div id="navigation">
-            <ul>
-                <p><strong>Links</strong></p>
-                <li><a href="http://mck1304963cwresit.azurewebsites.net/Welcome.php">Return to the welcome page</a></li>
-                <li><a href="http://mck1304963cwresit.azurewebsites.net/Register.php">Create a new account</a></li>
-                <li><a href="http://mck1304963cwresit.azurewebsites.net/BugCreationForm.php">Create a new Bug</a></li>
-                <li><a href="http://mck1304963cwresit.azurewebsites.net/Search.php">Search</a></li>
-            </ul>
+                $Search = "SELECT user_ID, name FROM Users WHERE name = '$name' AND user_ID = '$Pass'";
+                $row = mysqli_fetch_array($Search) or die(mysqli_error($conn));
+                    if(!empty($row['username']) AND !empty($row['password'])) {
+                        $_SESSION['Login'] = $row['username'];
+                        echo "correct login";
+                    }else {
+                        echo "Incorrect login, please retry";
+                    }
+                    mysqli_close($conn);
+                ?>
             </div>
-        <div id="content">
-            <div id="container">
-        
+            <div id="navigation">
+                <ul>
+                    <p><strong>Links</strong></p>
+                    <li><a href="http://mck1304963cwresit.azurewebsites.net/Welcome.php">Return to the welcome page</a></li>
+                    <li><a href="http://mck1304963cwresit.azurewebsites.net/Register.php">Create a new account</a></li>
+                    <li><a href="http://mck1304963cwresit.azurewebsites.net/BugCreationForm.php">Create a new Bug</a></li>
+                    <li><a href="http://mck1304963cwresit.azurewebsites.net/Search.php">Search</a></li>
+                </ul>
             </div>
-        <div id="comments">
-            <p><strong>Comments</strong></p>
-        </div>
-        <div id="footer">
-            <p><strong>Legal shit</strong></p>
-        </div>
-    </body>
+            <div id="content">
+                <div id="container"></div>
+            <div id="comments">
+                <p><strong>Comments</strong></p>
+            </div>
+            <div id="footer">
+                <p><strong>Legal stuff</strong></p>
+            </div>
+        </body>
 </html>
