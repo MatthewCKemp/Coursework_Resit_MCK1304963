46ebd142cc96602459f4876ef7bb22c22895b028
MatthewCKemp <matthewconorkemp@gmail.com>
2016-07-08 03:26:45 +0100

    BCF_V.2.7.1

--- BugCreationForm.php
+++ BugCreationForm.php
@@ -4,6 +4,25 @@
         <meta charset="UTF-8">
         <title>BugSplat: Create a New Bug</title>
         <link rel="stylesheet" type ="text/css" href="layout.css" />
+        <?php
+        $username = "b56f549a76a983";
+        $password = "a3035583";
+        $servername = "us-cdbr-azure-west-c.cloudapp.net";
+
+        // Create connection to DB
+        $conn = mysqli_connect($servername, $username, $password);
+        $select = mysqli_select_db($conn, 'mck1304963_cwrs_db');
+        // Check the connectionas
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        //echo "Connected successfully";
+        if (!$select) {
+            die(" Selection failed: " . mysqli_connect_error());
+        }
+        //echo " DB Selected successfully";
+
+        ?>
     </head>
         <body>
             <div id ="header">Create A New Bug</div>
@@ -52,25 +71,5 @@
             <div id="footer">
                 <p><strong>Legal shit</strong></p>
             </div>
-            
-            <?php
-            $username = "b56f549a76a983";
-            $password = "a3035583";
-            $servername = "us-cdbr-azure-west-c.cloudapp.net";
-
-            // Create connection to DB
-            $conn = mysqli_connect($servername, $username, $password);
-            $select = mysqli_select_db($conn, 'mck1304963_cwrs_db');
-            // Check the connectionas
-            if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
-            //echo "Connected successfully";
-            if (!$select) {
-                die(" Selection failed: " . mysqli_connect_error());
-            }
-            //echo " DB Selected successfully";
-            
-            ?>
         </body>
 </html>
