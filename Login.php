5c77a1453b681c71e42655314041958555f09669
MatthewCKemp <matthewconorkemp@gmail.com>
2016-07-08 03:04:42 +0100

    Login_V.2.13.1

--- Login.php
+++ Login.php
@@ -26,56 +26,52 @@
     </head>
 
     <body>
-    <div id ="header">Login</div>
-    <div id="login">
-        <form action="Login.php" method="post">
-            Username: <input type="text" name="username"><br>
-            Password: <input type="password" name="password"><br>
-            <input type="submit" value ="Login"/><br/>
-        </form>
-        <?php
-            if("POST") { //More secure than GET
-
-                $name = mysqli_real_escape_string($conn,$_POST['username']);
-                $Pass = mysqli_real_escape_string($conn,$_POST['password']);
-
-                $Search = "SELECT user_ID, name FROM Users WHERE name = '$name' AND user_ID = '$Pass'";
-                $row = mysqli_fetch_array($Search) or die(mysqli_error($conn));
-
-
-                    if(!empty($row['username']) AND !empty($row['password'])) {
-
-                    $_SESSION['Login'] = $row['username'];
-                    echo "correct login";
-
-                    }else {
-                    echo "Incorrect login, please retry";
-                    }
-            }
-            mysqli_close($conn);
-        ?>
-
-    </div>
-    <div id="navigation">
-        <ul>
-            <p><strong>Links</strong></p>
-            <li><a href="http://mck1304963cwresit.azurewebsites.net/Welcome.php">Return to the welcome page</a></li>
-            <li><a href="http://mck1304963cwresit.azurewebsites.net/Register.php">Create a new account</a></li>
-            <li><a href="http://mck1304963cwresit.azurewebsites.net/BugCreationForm.php">Create a new Bug</a></li>
-            <li><a href="http://mck1304963cwresit.azurewebsites.net/Search.php">Search</a></li>
-        </ul>
-    </div>
-    <div id="content">
-        <p><strong>Main writing</strong></p>
-    </div>
-    <div id="comments">
-        <p><strong>Comments</strong></p>
-    </div>
-    <div id="footer">
-        <p><strong>Legal shit</strong></p>
-    </div>
-
-
-
+        <div id ="header">Login</div>
+        <div id="login">
+            <form action="Login.php" method="post">
+                Username: <input type="text" name="username"><br>
+                Password: <input type="password" name="password"><br>
+                <input type="submit" value ="Login"/><br/>
+            </form>
+            <?php
+                if("POST") { //More secure than GET
+    
+                    $name = mysqli_real_escape_string($conn,$_POST['username']);
+                    $Pass = mysqli_real_escape_string($conn,$_POST['password']);
+    
+                    $Search = "SELECT user_ID, name FROM Users WHERE name = '$name' AND user_ID = '$Pass'";
+                    $row = mysqli_fetch_array($Search) or die(mysqli_error($conn));
+    
+    
+                        if(!empty($row['username']) AND !empty($row['password'])) {
+    
+                        $_SESSION['Login'] = $row['username'];
+                        echo "correct login";
+    
+                        }else {
+                        echo "Incorrect login, please retry";
+                        }
+                }
+                mysqli_close($conn);
+            ?>
+        </div>
+        <div id="navigation">
+            <ul>
+                <p><strong>Links</strong></p>
+                <li><a href="http://mck1304963cwresit.azurewebsites.net/Welcome.php">Return to the welcome page</a></li>
+                <li><a href="http://mck1304963cwresit.azurewebsites.net/Register.php">Create a new account</a></li>
+                <li><a href="http://mck1304963cwresit.azurewebsites.net/BugCreationForm.php">Create a new Bug</a></li>
+                <li><a href="http://mck1304963cwresit.azurewebsites.net/Search.php">Search</a></li>
+            </ul>
+            </div>
+        <div id="content">
+            <p><strong>Main writing</strong></p>
+        </div>
+        <div id="comments">
+            <p><strong>Comments</strong></p>
+        </div>
+        <div id="footer">
+            <p><strong>Legal shit</strong></p>
+        </div>
     </body>
 </html>
