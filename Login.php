a74a714e97aab1501c23d5f8821df42f8ab167d4
MatthewCKemp <matthewconorkemp@gmail.com>
2016-07-12 01:21:12 +0100

    Login_V.3.3.1

--- Login.php
+++ Login.php
@@ -37,7 +37,7 @@
             </div>
             <div id="content">
                 <div id="container">
-                    <form method="post"> action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
                         <label>Username: </label><input type="text" name="username"/><br><br/>
                         <label>Password: </label><input type="password" name="password"/><br><br/>
                         <input type="submit" value ="Login"/><br/>
