ef4befb543d4f8767ef9ea42709430ac97fa35c9
MatthewCKemp <matthewconorkemp@gmail.com>
2016-07-14 01:36:52 +0100

    Template_V.0.1

---
+++ Template.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang=en">
+    <head>
+        <meta charset="UTF-8">
+        <title>BugSplat Login</title>
+        <link rel="stylesheet" type ="text/css" href="layout.css" />
+        <?php
+        $username = "b56f549a76a983";
+        $password = "a3035583";
+        $servername = "us-cdbr-azure-west-c.cloudapp.net";
+
+        // Create connection to DB
+        $conn = mysqli_connect($servername, $username, $password);
+        $select = mysqli_select_db($conn, 'mck1304963_cwrs_db');
+        // Check the connectionas
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        //echo "Connected successfully";
+        if (!$select) {
+            die(" Selection failed: " . mysqli_connect_error());
+        }
+        //echo " DB Selected successfully";
+        session_start();
+        ?>
+    </head>
+    <body>
+        <div id ="header">Login</div>
+        <div id="navigation">
+            <ul>
+                <p><strong>Links</strong></p>
+                <li><a href="http://mck1304963cwresit.azurewebsites.net/Welcome.php">Return to the welcome page</a></li>
+                <li><a href="http://mck1304963cwresit.azurewebsites.net/Register.php">Create a new account</a></li>
+                <li><a href="http://mck1304963cwresit.azurewebsites.net/BugCreationForm.php">Create a new Bug</a></li>
+                <li><a href="http://mck1304963cwresit.azurewebsites.net/Search.php">Search</a></li>
+            </ul>
+        </div>
+        <div id="content">
+            <div id="container">
+            </div>
+        </div>
+        <div id="comments">
+            <p><strong>Comments</strong></p>
+        </div>
+        <div id="footer">
+            <p><strong>Legal stuff</strong></p>
+        </div>
+    </body>
+</html>
