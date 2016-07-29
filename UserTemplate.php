af9c28f2d89c9498b2a9d746af13ca9ce13e6f01
MatthewCKemp <matthewconorkemp@gmail.com>
2016-07-29 02:35:30 +0100

    UserTemplate_V.1.11

--- UserTemplate.php
+++ UserTemplate.php
@@ -40,10 +40,9 @@
 </div>
 <div id="content">
     <div id="container">
-        <!--Bug info + creator and location-->
         <?php
         $devID = $_GET["devID"];
-        $UserQuery = "SELECT Bugs.title, Bugs.description, Bugs.bugposted, Bugs.bugfixed, Bugs.status, Users.name, Users.country FROM Bugs, Users WHERE Bugs.user_ID LIKE Users.user_ID AND Users.user_ID LIKE $devID ";
+        $UserQuery = "SELECT Users.name, Users.country, Bugs.title, Bugs.bugposted FROM Users, Bugs WHERE Users.user_ID LIKE Bugs.user_ID AND Users.user_ID LIKE $devID ";
         $UserResult = mysqli_query($conn, $UserQuery);
         
         if (mysqli_num_rows($UserResult) > 0 ){
@@ -52,7 +51,7 @@
             }
             echo"</table>";
         } else {
-            echo "An error has occurred. This bug does not exist." . mysqli_error($conn);
+            echo "An error has occurred. This user does not exist." . mysqli_error($conn);
         }
         mysqli_close($conn);
         ?>
