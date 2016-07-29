1776089fc4ea9de194278f9954a7aba5354347ce
MatthewCKemp <matthewconorkemp@gmail.com>
2016-07-29 02:27:58 +0100

    UserTemplate_V.1.10

--- UserTemplate.php
+++ UserTemplate.php
@@ -43,7 +43,7 @@
         <!--Bug info + creator and location-->
         <?php
         $devID = $_GET["devID"];
-        $UserQuery = "SELECT Users.name, Users.country, Bugs.title, Bugs.bugposted FROM Users, Bugs WHERE Users.user_ID LIKE Bugs.user_ID AND Users.user_ID LIKE $devID ";
+        $UserQuery = "SELECT Users.name, Users.country, Bugs.title, Bugs.bugposted FROM Users, Bugs WHERE Users.user_ID LIKE Bugs.user_ID AND Users.user_ID LIKE '$devID' ";
         $UserResult = mysqli_query($conn, $UserQuery);
         
         if (mysqli_num_rows($UserResult) > 0 ){
